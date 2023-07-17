41c9853d3b67a24b5dd3be0939921e3d7b8d0d77
LeeChoongHwan <choo901@naver.com>
2023-07-17 12:49:25 +0900

    step1

---
+++ app/src/main/java/com/example/calculator_kotlin/Calculator.kt
@@ -0,0 +1,22 @@
+package com.example.calculator_kotlin
+
+class Calculator(num1 : Int, num2 : Int) {
+    var num1 = 0;
+    var num2 = 0;
+    init {
+        this.num1 = num1
+        this.num2 = num2
+    }
+    fun add() : Int {
+        return num1 + num2;
+    }
+    fun subtraction() : Int {
+        return num1 - num2;
+    }
+    fun multiply() : Int {
+        return num1 * num2;
+    }
+    fun divide() : Int {
+        return num1 / num2;
+    }
+}
--- app/src/main/java/com/example/calculator_kotlin/Main.kt
+++ app/src/main/java/com/example/calculator_kotlin/Main.kt
@@ -2,4 +2,34 @@ package com.example.calculator_kotlin
 
 fun main() {
 
+    println("2개의 숫자(정수)를 입력하세요")
+
+    var num1 = readLine()!!.toInt()
+    var num2 = readLine()!!.toInt()
+
+    var calculator = Calculator(num1, num2)
+
+    println("연산을 선택하세요")
+    println("1. 덧셈")
+    println("2. 뺄셈")
+    println("3. 곱셈")
+    println("4. 나눗셈")
+
+    var operation = readLine()!!.toInt()
+
+    when(operation) {
+        1 -> {
+            println(calculator.add())
+        }
+        2 -> {
+            println(calculator.subtraction())
+        }
+        3 -> {
+            println(calculator.multiply())
+        }
+        4 -> {
+            println(calculator.divide())
+        }
+    }
+
 }

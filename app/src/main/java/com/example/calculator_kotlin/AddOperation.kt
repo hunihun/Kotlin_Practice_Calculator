47c7333ae8af6ba00ce12ce330a6877619ae7b38
LeeChoongHwan <choo901@naver.com>
2023-07-17 15:40:36 +0900

    code refactoring

--- app/src/main/java/com/example/calculator_kotlin/AddOperation.kt
+++ app/src/main/java/com/example/calculator_kotlin/AddOperation.kt
@@ -1,5 +1,5 @@
 package com.example.calculator_kotlin
 
 class AddOperation : Operation() {
-    override fun operate(num1: Double, num2: Double): Double = (num1 + num2)
+    override fun operate(num1: Double, num2: Double): Double = num1 + num2
 }

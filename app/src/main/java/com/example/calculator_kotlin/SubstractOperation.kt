a9b298fefdfda76fee39c6ce83bf74c2f74f2bf7
LeeChoongHwan <choo901@naver.com>
2023-07-17 15:32:37 +0900

    level 4

--- app/src/main/java/com/example/calculator_kotlin/SubstractOperation.kt
+++ app/src/main/java/com/example/calculator_kotlin/SubstractOperation.kt
@@ -1,5 +1,5 @@
 package com.example.calculator_kotlin
 
-class SubstractOperation : Operation{
+class SubstractOperation : Operation(){
     override fun operate(num1: Double, num2: Double): Double = (num1 - num2)
 }

b5e2567f3b109413a6d091cda3df3b8c3d091994
LeeChoongHwan <choo901@naver.com>
2023-07-19 19:12:43 +0900

    three numbers

--- app/src/main/java/com/example/calculator_kotlin/Calculator.kt
+++ app/src/main/java/com/example/calculator_kotlin/Calculator.kt
@@ -1,7 +1,8 @@
 package com.example.calculator_kotlin
 
 class Calculator() {
-    fun operate(num1 : Double, num2 : Double, operation: Operation): Double {
-        return operation.operate(num1,num2)
+    fun operate(num1 : Double, num2 : Double, num3 : Double, operation1: Operation, operation2: Operation): Double {
+        var firstOperation =  operation1.operate(num1,num2)
+        return operation2.operate(firstOperation,num3)
     }
 }
--- app/src/main/java/com/example/calculator_kotlin/Main.kt
+++ app/src/main/java/com/example/calculator_kotlin/Main.kt
@@ -1,39 +1,62 @@
 package com.example.calculator_kotlin
 
 fun main() {
+    while (true) {
+        println("첫 번째 숫자를 입력하세요.")
+        var n1 = readLine()!!.toDouble()
+        chooseOperation()
+        var operation1 = readLine()
+        println("두 번째 숫자를 입력하세요.")
+        var n2 = readLine()!!.toDouble()
+        chooseOperation()
+        var operation2 = readLine()
+        println("세 번째 숫자를 입력하세요.")
+        var n3 = readLine()!!.toDouble()
 
-    println("2개의 숫자를 입력하세요")
+        var calc = Calculator()
 
-    var num1 = readLine()!!.toDouble()
-    var num2 = readLine()!!.toDouble()
-
-    println("연산을 선택하세요")
-    println("1. 덧셈")
-    println("2. 뺄셈")
-    println("3. 곱셈")
-    println("4. 나눗셈")
-
-    var operation = readLine()!!.toInt()
-
-    var calc = Calculator()
-
-    when(operation) {
-        1 -> {
-            println(calc.operate(num1, num2, AddOperation()))
+        if (operation2 == "1") {
+            when (operation1) {
+                "1" -> println(calc.operate(n1, n2, n3, AddOperation(), AddOperation()))
+                "2" -> println(calc.operate(n1, n2, n3, SubtractOperation(), AddOperation()))
+                "3" -> println(calc.operate(n1, n2, n3, MultiplyOperation(), AddOperation()))
+                "4" -> println(calc.operate(n1, n2, n3, DivideOperation(), AddOperation()))
+                else -> println("올바른 연산을 입력하세요.")
+            }
+        } else if (operation2 == "2") {
+            when (operation1) {
+                "1" -> println(calc.operate(n1, n2, n3, AddOperation(), SubtractOperation()))
+                "2" -> println(calc.operate(n1, n2, n3, SubtractOperation(), SubtractOperation()))
+                "3" -> println(calc.operate(n1, n2, n3, MultiplyOperation(), SubtractOperation()))
+                "4" -> println(calc.operate(n1, n2, n3, DivideOperation(), SubtractOperation()))
+                else -> println("올바른 연산을 입력하세요.")
+            }
+        } else if (operation2 == "3") {
+            when (operation1) {
+                "1" -> println(calc.operate(n2, n3, n1, MultiplyOperation(), AddOperation()))
+                "2" -> println(-(calc.operate(n2, n3, n1, MultiplyOperation(), SubtractOperation())))
+                "3" -> println(calc.operate(n1, n2, n3, MultiplyOperation(), MultiplyOperation()))
+                "4" -> println(calc.operate(n1, n2, n3, DivideOperation(), MultiplyOperation()))
+                else -> println("올바른 연산을 입력하세요.")
+            }
+        } else if (operation2 == "4") {
+            when (operation1) {
+                "+" -> println(calc.operate(n2, n3, n1, DivideOperation(), AddOperation()))
+                "-" -> println(-(calc.operate(n2, n3, n1, DivideOperation(), SubtractOperation())))
+                "*" -> println(calc.operate(n1, n2, n3, MultiplyOperation(), DivideOperation()))
+                "/" -> println(calc.operate(n1, n2, n3, DivideOperation(), DivideOperation()))
+                else -> println("올바른 연산을 입력하세요.")
+            }
         }
-        2 -> {
-            println(calc.operate(num1, num2, SubtractOperation()))
-        }
-        3 -> {
-            println(calc.operate(num1, num2, MultiplyOperation()))
-        }
-        4 -> {
-            println(calc.operate(num1, num2, DivideOperation()))
-        }
-        else -> {
-            println("올바른 번호를 입력하세요.")
+        else {
+            println("올바른 연산을 입력하세요.")
         }
     }
-
 }
-
+private fun chooseOperation() {
+    println("연산을 숫자로 입력하세요.")
+    println("1. +")
+    println("2. -")
+    println("3. *")
+    println("4. /")
+}

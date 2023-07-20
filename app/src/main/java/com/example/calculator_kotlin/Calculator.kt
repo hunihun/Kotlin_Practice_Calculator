01399e06c763e7c0d74b8496ce84ea4708f1df44
jsh <shjung@lifegoeson.kr>
2023-07-20 11:50:36 +0900

    refactor: code refactoring

--- app/src/main/java/com/example/calculator_kotlin/Calculator.kt
+++ app/src/main/java/com/example/calculator_kotlin/Calculator.kt
@@ -5,4 +5,38 @@ class Calculator() {
         var firstOperation =  operation1.operate(num1,num2)
         return operation2.operate(firstOperation,num3)
     }
+
+    fun operate(inputs: List<Double>, operations: List<Operation>): Double {
+        val newInputs = inputs.toMutableList()
+        val newOperations = operations.toMutableList()
+
+        while (newOperations.find {
+                it is MultiplyOperation || it is DivideOperation
+            } != null) {
+            val operation = newOperations.find {
+                it is MultiplyOperation || it is DivideOperation
+            } ?: break
+            val index = newOperations.indexOf(operation)
+            newInputs[index] = operation.operate(newInputs[index], newInputs[index + 1])
+            newInputs.removeAt(index + 1)
+            newOperations.removeAt(index)
+            println("newInputs >>> $newInputs")
+        }
+
+        while (newOperations.find {
+                it is SubtractOperation || it is AddOperation
+            } != null) {
+            val operation = newOperations.find {
+                it is SubtractOperation || it is AddOperation
+            } ?: break
+            val index = newOperations.indexOf(operation)
+            newInputs[index] = operation.operate(newInputs[index], newInputs[index + 1])
+            newInputs.removeAt(index + 1)
+            newOperations.removeAt(index)
+            println("newInputs2 >>> $newInputs")
+        }
+
+        return newInputs[0]
+    }
 }
+
